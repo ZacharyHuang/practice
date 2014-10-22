43a27b6f7013c7b3164a8de7c64e6dc10cf4a6ce
ZacharyHuang <244502111@qq.com>
2014-10-22 11:08:42 +0800

    Memory Limit Exceeded

--- p1009.c
+++ p1009.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 typedef unsigned char byte;
 byte rdata = 0;
-int  rlen = 0;
+long  rlen = 0;
 byte ddata = 0;
-int  dlen = 0;
+long  dlen = 0;
 struct line_struct
 {
 	byte *p;
@@ -13,40 +13,39 @@ struct line_struct
 	struct line_struct *prior;
 };
 
-int push(struct line_struct *l, int size)
+int push(byte *p, int size)
 {
 	if (rlen < 0) {
 		printf("rlen<0\n");
 		exit(-1);
 	}
 	if (rlen == 0) {
-		scanf("%d %d", &rdata, &rlen);
+		scanf("%d %ld", &rdata, &rlen);
 		if (rdata == 0 && rlen == 0) {
-			memcpy(l->p, l->prior->p, size);
 			return 0;
 		}
 	}
 	if (rlen > 0) {
 		if (rlen >= size) {
-			memset(l->p, rdata, size);
+			if (ddata == 0 && dlen >= size && rdata == p[0]) {
+				dlen += rlen;
+				rlen %= size;
+				dlen -= rlen;
+				return push(p, size);
+			}
+			memset(p, rdata, size);
 			rlen -= size;
 		} else {
-			memset(l->p, rdata, rlen);
+			memset(p, rdata, rlen);
+			size -= rlen;
+			p += rlen;
 			rlen = 0;
-			push(l, size - rlen);
+			return push(p, size);
 		}
 	}
 	return 1;
 }
 #define dist(a,b) a>b?a-b:b-a
-/*byte dist(byte a, byte b)
-{
-	if (a>b) {
-		return a-b;
-	} else {
-		return b-a;
-	}
-}*/
 byte getdata(struct line_struct *l, int i, int size)
 {
 	byte max = 0;
@@ -77,12 +76,15 @@ byte getdata(struct line_struct *l, int i, int size)
 void deal(struct line_struct *l, int size)
 {
 	int i = 0;
+
 	for (i = 0; i < size; i ++) {
 		byte data = getdata(l, i, size);
 		if (data == ddata) {
 			dlen ++;
 		} else {
-			printf("%d %d\n", ddata, dlen);
+			if (dlen > 0) {
+				printf("%d %ld\n", ddata, dlen);
+			}
 			ddata = data;
 			dlen = 1;
 		}
@@ -90,6 +92,17 @@ void deal(struct line_struct *l, int size)
 }
 int main (int argc, char *argv[])
 {
+	struct line_struct ll[3];
+		ll[0].p = (byte *)calloc(1, sizeof(byte));
+		ll[0].next = &ll[1];
+		ll[0].prior = &ll[2];
+		ll[1].p = (byte *)calloc(1, sizeof(byte));
+		ll[1].next = &ll[2];
+		ll[1].prior = &ll[0];
+		ll[2].p = (byte *)calloc(1, sizeof(byte));
+		ll[2].next = &ll[0];
+		ll[2].prior = &ll[1];
+		struct line_struct * l = &ll[0];
 	while (1) {
 		rdata = 0;
 		rlen = 0;
@@ -101,28 +114,24 @@ int main (int argc, char *argv[])
 		if (size == 0) {
 			break;
 		}
-		struct line_struct ll[3];
-		ll[0].p = (byte *)calloc(size, sizeof(byte));
-		ll[0].next = &ll[1];
-		ll[0].prior = &ll[2];
-		ll[1].p = (byte *)calloc(size, sizeof(byte));
-		ll[1].next = &ll[2];
-		ll[1].prior = &ll[0];
-		ll[2].p = (byte *)calloc(size, sizeof(byte));
-		ll[2].next = &ll[0];
-		ll[2].prior = &ll[1];
-		struct line_struct * l = &ll[0];
+		ll[0].p = (byte *)realloc(ll[0].p, size*sizeof(byte));
+		ll[1].p = (byte *)realloc(ll[1].p, size*sizeof(byte));
+		ll[2].p = (byte *)realloc(ll[2].p, size*sizeof(byte));
 
-		push(l, size);
+		push(l->p, size);
 		memcpy(l->prior->p, l->p, size);
 		l = l->next;
-		while (push(l, size)) {
-			deal(l, size);
+		while (push(l->p, size)) {
+			deal(l->prior, size);
 			l = l->next;
 		}
+		memcpy(l->p, l->prior->p, size);
 		l = l->prior;
-		deal(l, size);
-		printf("%d %d\n0 0\n", ddata, dlen);
+		deal(l->prior, size);
+		printf("%d %ld\n0 0\n", ddata, dlen);
 	}
+		free(ll[0].p);
+		free(ll[1].p);
+		free(ll[2].p);
 	return 0;
 }
