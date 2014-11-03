6aa31cb07725c1da45304d2679f02eb1ad73a53c
ZacharyHuang <244502111@qq.com>
2014-11-03 23:52:42 +0800

    wa and debuging

--- p1159.c
+++ p1159.c
@@ -1,63 +1,79 @@
 #include <stdio.h>
+#include <string.h>
+
+#define lf 1<<30
+#define rf 1<<29
+#define im 0x3FFF<<14
+#define lm 0x3FFF
+
 int s2i(char s)
 {
 	if (s<='9' && s>='0') {
 		return (s-'0');
 	}
-	if (s<='A' && s>='Z') {
+	if (s>='A' && s<='Z') {
 		return (s - 'A' + 10);
 	}
 	return (s - 'a' + 36);
 }
-int find_one_pair(char *s, int start, int end)
+int find_one_pair(char* s, int start, int end)
 {
 	int i = start;
 	int j = end;
-	int res = 0;
-	int lc[62], rc[62];
-	memset(lc, -1, 62);
-	memset(rc, -1, 62);
-	while (j - i > 1) {
+	int c[62];
+	memset(c, 0, 62 * sizeof(int));
+	while (i < j) {
 		int li = s2i(s[i]);
-		if (rc[li] > 0) {
-			res += i - start + end - rc[li];
-			start = i + 1;
-			end = rc[li] - 1;
+		if (c[li] & rf) {
+			return 2 + find_one_pair(s, i, (c[li]&im)>>14);
 		}
-		if (lc[li] < 0) {
-			lc[li] = i;
+		if (c[li] & lf) {
+			c[li] |= i - ((c[li]&im) >> 14);
+		} else {
+			c[li] = i<<14;
+			c[li] |= lf;
 		}
-		i ++;
-		if (j - i > 1) {
+		i++;
+		if (j <= i ) {
 			break;
 		}
 		int ri = s2i(s[j]);
-		if (lc[ri] > 0) {
-			res += lc[ri] - start + end - j;
-			start = lc[ri] + 1;
-			end = j - 1;
-			return res;
+		if (c[li] & lf) {
+			return 2 + find_one_pair(s, (c[li]&im)>>14, j);
 		}
-		if (rc[ri] < 0) {
-			rc[ri] = j;
+		if (c[li] & rf) {
+			c[li] |= ((c[li]&im) >> 14) - j;
+		} else {
+			c[li] = i<<14;
+			c[li] |= rf;
 		}
-		j --;
+		j--;
 	}
-	res += start - end - 1;
+	int max = 0;
+	int tmp;
+	for (i = 0; i < 62; i ++) {
+		if (c[i]&lm) {
+			j = (c[i]&im)>>14;
+			if (c[i]&lf) {
+				tmp = find_one_pair(s, j, j+c[i]&lm);
+			} else if (c[i]&rf) {
+				tmp = find_one_pair(s, j-c[i]&lm, j);
+			}
+			if (tmp > max) {
+				max = tmp;
+			}
+		}
+	}
+	return max;
 }
 int main (int argc, char *argv[])
 {
-
 	int len;
-	char str[5001];
-	scanf("%d", len);
-	scanf("%s", str);
-	int res = 0, count = 0;
-	int i = 0, j = len - 1;
-	while ((res = find_one_pair(str, &i, &j)) >= 0) {
-		count += res;
-	}
-	count -= res;
-	printf("%d\n",res);
+	char s[5001];
+	scanf("%d", &len);
+	scanf("%s", s);
+	int res = find_one_pair(s, 0, len-1);
+	
+	printf("%d\n",len-res-1);
 	return 0;
 }
